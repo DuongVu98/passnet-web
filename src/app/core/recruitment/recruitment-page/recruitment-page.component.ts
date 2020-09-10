47322f373df74d1c39aa37dc4b0078526410edd7
Tung Duong <duongvutung1998@gmail.com>
2020-09-10 15:52:17 +0700

    install some libs

---
+++ src/app/core/recruitment/recruitment-page/recruitment-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "recruitment-recruitment-page",
+	templateUrl: "./recruitment-page.component.html",
+	styleUrls: ["./recruitment-page.component.scss"],
+})
+export class RecruitmentPageComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
--- src/app/core/recruitment/recruitment.module.ts
+++ src/app/core/recruitment/recruitment.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RecruitmentPageComponent } from "./recruitment-page/recruitment-page.component";
 
 @NgModule({
-	declarations: [],
+	declarations: [RecruitmentPageComponent],
 	imports: [CommonModule],
+	exports: [RecruitmentPageComponent],
 })
 export class RecruitmentModule {}
