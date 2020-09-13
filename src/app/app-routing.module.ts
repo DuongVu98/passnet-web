29af178c71fdcd27e307062ee3f9b27b3158d9ea
Tung Duong <duongvutung1998@gmail.com>
2020-09-13 23:38:48 +0700

    apply routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./core/auth/login/login.component";
+import { ProfilePageComponent } from "./core/profile/profile-page/profile-page.component";
+import { RecruitmentPageComponent } from "./core/recruitment/recruitment-page/recruitment-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "",
+		component: RecruitmentPageComponent,
+	},
+	{
+		path: "login",
+		component: LoginComponent,
+	},
+	{
+		path: "profile",
+		component: ProfilePageComponent,
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
