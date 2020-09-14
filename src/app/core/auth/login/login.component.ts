22eb1dc4997ebc09d6105154663415211cdcc059
Tung Duong <duongvutung1998@gmail.com>
2020-09-14 14:49:23 +0700

    apply router

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { LoginService } from "../services/login.service";
+import { Router } from "@angular/router";
 
-import { SetLoggedUserAction } from "../store/auth.actions";
-import { UserModel } from "../models/auth.models";
+import { LoginService } from "../services/login.service";
 
 @Component({
 	selector: "auth-login",
@@ -13,20 +12,23 @@ export class LoginComponent implements OnInit {
 	email: string;
 	password: string;
 
-	constructor(private loginService: LoginService) {}
+	constructor(private loginService: LoginService, private router: Router) {}
 
 	ngOnInit(): void {}
 
 	login(): void {
 		console.log(`email: ${this.email} - password: ${this.password}`);
 		this.loginService.login(this.email, this.password);
+		this.router.navigate(["/"]);
 	}
 
 	googleLogin(): void {
-		this.loginService.loginWithGoogle();
+        this.loginService.loginWithGoogle();
+        this.router.navigate(["/"]);
 	}
 
 	logout(): void {
-		this.loginService.logout();
+        this.loginService.logout();
+        this.router.navigate(["/"]);
 	}
 }
