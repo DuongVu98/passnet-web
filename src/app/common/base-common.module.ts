6fa4afc2a760fd31d97256c3b387bab007c1519c
Tung Duong <duongvutung1998@gmail.com>
2020-09-21 12:06:01 +0700

    resize import

--- src/app/common/base-common.module.ts
+++ src/app/common/base-common.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ApiModule } from "./api/api.module";
 import { FirebaseAppModule } from "./firebase-auth/firebase-app.module";
-import { MDBBootstrapModule } from "angular-bootstrap-md";
 import { FormsModule } from "@angular/forms";
-
+import { MdbModule } from "./mdb/mdb.module";
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, FormsModule, ApiModule, FirebaseAppModule, MDBBootstrapModule.forRoot()],
-	exports: [CommonModule, FormsModule, ApiModule, FirebaseAppModule, MDBBootstrapModule],
+	imports: [CommonModule, FormsModule, ApiModule, FirebaseAppModule, MdbModule],
+	exports: [CommonModule, FormsModule, ApiModule, FirebaseAppModule, MdbModule],
 })
 export class BaseCommonModule {}
---
+++ src/app/common/mdb/mdb.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IconsModule, NavbarModule } from "angular-bootstrap-md";
+
+@NgModule({
+	declarations: [],
+	imports: [CommonModule],
+	exports: [IconsModule, NavbarModule],
+})
+export class MdbModule {}
