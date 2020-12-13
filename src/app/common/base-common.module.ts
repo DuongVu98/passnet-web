e854bf8df0cd4219f99cabc723e39d516220b067
Tung Duong <duongvutung1998@gmail.com>
2020-12-13 23:24:33 +0700

    remove firebase

--- src/app/common/base-common.module.ts
+++ src/app/common/base-common.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ApiModule } from "./api/api.module";
-import { FirebaseAppModule } from "./firebase-auth/firebase-app.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MdbModule } from "./mdb/mdb.module";
 import { MaterialModule } from "./material/material.module";
@@ -13,7 +12,7 @@ import { LogoutPublisher } from "./publishers/logout.publisher";
 
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, ApiModule, FirebaseAppModule, MdbModule, MaterialModule],
+	imports: [CommonModule, FormsModule, ReactiveFormsModule, ApiModule, MdbModule, MaterialModule],
 	providers: [
 		LogoutPublisher,
 		{
@@ -27,6 +26,6 @@ import { LogoutPublisher } from "./publishers/logout.publisher";
 			multi: true,
 		},
 	],
-	exports: [CommonModule, FormsModule, ReactiveFormsModule, ApiModule, FirebaseAppModule, MdbModule, MaterialModule],
+	exports: [CommonModule, FormsModule, ReactiveFormsModule, ApiModule, MdbModule, MaterialModule],
 })
 export class BaseCommonModule {}
