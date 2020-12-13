faa6cbf74db02ecfd1358c0eb98998423515d747
Tung Duong <duongvutung1998@gmail.com>
2020-12-13 23:45:53 +0700

    remove firebase module

--- src/app/common/base-common.module.ts
+++ src/app/common/base-common.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ApiModule } from "./api/api.module";
-import { FirebaseAppModule } from "./firebase-auth/firebase-app.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MdbModule } from "./mdb/mdb.module";
 import { MaterialModule } from "./material/material.module";
