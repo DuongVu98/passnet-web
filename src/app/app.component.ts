ac271620dad599ed3d93df3cd849f7e498accee6
DuongVu98 <duongvutung1998@gmail.com>
2021-06-23 15:29:09 +0700

    clean codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Router } from "@angular/router";
 import { Store } from "@ngxs/store";
 import { OktaAuthService } from "@okta/okta-angular";
 import { Builder } from "builder-pattern";
-import { LogoutPublisher } from "./common/publishers/logout.publisher";
 import { UserModel } from "./core/auth/models/auth.models";
 import { SetLoggedUserAction } from "./core/auth/store/auth.actions";
 
@@ -35,13 +34,6 @@ export class AppComponent implements OnInit {
 	async ngOnInit() {
 		this.isAuthenticated = await this.oktaAuth.isAuthenticated();
 		console.log(`isAuthenticated: ${this.isAuthenticated}`);
-	}
-
-	login() {
-		this.oktaAuth.signInWithRedirect({
-			originalUri: "/",
-		});
-		console.log("logged from app");
 	}
 
 	async logout() {
