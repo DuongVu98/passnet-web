9a20b9a983ce958387d9b5942bbeffd83d5f02e4
DuongVu98 <duongvutung1998@gmail.com>
2021-07-08 03:52:06 +0700

    refactor env

--- src/environments/environment.azure.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	production: false,
-	recruitmentApi: "http://20.81.200.40:8081",
-	authenticationApi: "http://13.72.86.164:1323",
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
 	production: true,
+	authenticationApi: "http://localhost:8080",
+	profileApi: "http://localhost:8081",
+	recruitmentApi: "http://localhost:8082",
+	classroomApi: "http://localhost:8083",
+	okta: {
+		domain: "dev-96211074.okta.com",
+		clientId: "0oa126nrgljPb0QHO5d7",
+		redirectUri: "http://localhost:4200/login/callback",
+	},
 };
