91aea9a17d88d50c920fd6ac365067c1fe0c9491
DuongVu98 <duongvutung1998@gmail.com>
2021-07-10 13:56:42 +0700

    add personal info

--- src/app/core/profile/personal-info/personal-info.component.ts
+++ src/app/core/profile/personal-info/personal-info.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProfileService } from "../services/profile.service";
+
+interface PersonalInfo {
+	fullName: string;
+	username: string;
+	email: string;
+}
 
 @Component({
-  selector: 'profile-personal-info',
-  templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.scss']
+	selector: "profile-personal-info",
+	templateUrl: "./personal-info.component.html",
+	styleUrls: ["./personal-info.component.scss"],
 })
 export class PersonalInfoComponent implements OnInit {
+	personalInfo: PersonalInfo;
 
-  constructor() { }
+	constructor(private profileService: ProfileService) {
+		this.personalInfo = {
+			fullName: "",
+			username: "",
+			email: "",
+		};
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.profileService.getPersonalInfo().subscribe((result) => {
+			this.personalInfo.fullName = result.fullName;
+			this.personalInfo.username = result.username;
+			this.personalInfo.email = result.email;
+		});
+	}
 }
