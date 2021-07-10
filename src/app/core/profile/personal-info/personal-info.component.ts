9cbd03dd8b98f460569fdc334b931993112671e2
DuongVu98 <duongvutung1998@gmail.com>
2021-07-10 13:15:47 +0700

    profile ui design

---
+++ src/app/core/profile/personal-info/personal-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-personal-info',
+  templateUrl: './personal-info.component.html',
+  styleUrls: ['./personal-info.component.scss']
+})
+export class PersonalInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
