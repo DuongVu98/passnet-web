b7db3898afbe7921689084f1761043165fc86a92
DuongVu98 <duongvutung1998@gmail.com>
2021-07-24 16:54:44 +0700

    production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
 	production: true,
-	authenticationApi: "http://localhost:8080",
-	profileApi: "http://localhost:8081",
-	recruitmentApi: "http://localhost:8082",
-	classroomApi: "http://localhost:8083",
-	organizerApi: "http://localhost:8084",
+	authenticationApi: "http://34.139.237.230/passet-user",
+	profileApi: "http://34.139.237.230/passet-user",
+	recruitmentApi: "http://34.139.237.230/passet-user",
+	classroomApi: "http://34.139.237.230/passet-user",
+	organizerApi: "http://34.139.237.230/passet-user",
 	okta: {
 		domain: "dev-96211074.okta.com",
 		clientId: "0oa126nrgljPb0QHO5d7",
-		redirectUri: "http://localhost:4200/login/callback",
+		redirectUri: "http://passnet.web.app/login/callback",
 	},
 };
