d4ab68a0e6febfca143c5b5de7cb928820be4e2c
DuongVu98 <duongvutung1998@gmail.com>
2021-07-24 18:01:32 +0700

    fix production hsots

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
 	production: true,
-	authenticationApi: "http://34.139.237.230/passet-user",
-	profileApi: "http://34.139.237.230/passet-profile",
-	recruitmentApi: "http://34.139.237.230/passet-recruitment",
-	classroomApi: "http://34.139.237.230/passet-classroom",
-	organizerApi: "http://34.139.237.230/passet-organizer",
+	authenticationApi: "http://34.139.237.230/passnet-user",
+	profileApi: "http://34.139.237.230/passnet-profile",
+	recruitmentApi: "http://34.139.237.230/passnet-recruitment",
+	classroomApi: "http://34.139.237.230/passnet-classroom",
+	organizerApi: "http://34.139.237.230/passnet-organizer",
 	okta: {
 		domain: "dev-96211074.okta.com",
 		clientId: "0oa126nrgljPb0QHO5d7",
