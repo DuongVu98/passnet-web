9038b10b6530fdfcf4342a5c07531d91d186a8d0
DuongVu98 <duongvutung1998@gmail.com>
2021-07-26 16:01:28 +0700

    refactor code

--- src/app/core/profile/profile.module.ts
+++ src/app/core/profile/profile.module.ts
@@ -14,7 +14,7 @@ import { ProfileState } from "./store/profile.state";
 import { BasicEditComponent } from "./basic-edit/basic-edit.component";
 import { AddExpFormComponent } from "./add-exp-form/add-exp-form.component";
 import { EditExpFormComponent } from "./edit-exp-form/edit-exp-form.component";
-import { ApplicationListComponent } from './application-list/application-list.component';
+import { ApplicationListComponent } from "./application-list/application-list.component";
 
 @NgModule({
 	declarations: [
